f4a7321573bf84f19a99ca0d95e846e48753e537
TamNhien <canhthien91@gmail.com>
2024-03-04 20:47:26 +0700

    Add project files.

---
+++ LTCB_Xuat1Den200.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 200; ++i) {
+        cout << i << " ";
+        // In mỗi số, sau đó thêm một khoảng trắng
+        if (i % 20 == 0) {
+            cout << endl; // Xuống dòng sau mỗi 20 số
+        }
+    }
+    return 0;
+}
